bce02a321064e6b363a0e15338ab95d036bd2289
Karan Nayal <somnathtigers2006@gmail.com>
2025-07-27 09:25:47 +0530

    code executed in conditional.cpp

---
+++ Basics/conditional.cpp
@@ -0,0 +1,68 @@
+// 1. if statement 
+/*
+int age = 18;
+if (age >= 18) {
+    std::cout << "You are an adult.";
+}
+// Output: You are an adult.
+*/
+
+
+
+
+// 2. if-else statement 
+/*
+int age = 16;
+if (age >= 18) {
+    std::cout << "You are an adult.";
+} else {
+    std::cout << "You are a minor.";
+}
+// Output: You are a minor.
+
+*/ 
+
+
+
+// 3. if-else else-if 
+/*
+int age = 16;
+if (age >= 18) {
+    std::cout << "You are an adult.";
+} else {
+    std::cout << "You are a minor.";
+}
+// Output: You are a minor.
+*/
+
+
+
+
+
+// 4. Switch statement 
+/*
+int day = 3;
+switch (day) {
+    case 1: std::cout << "Monday"; break;
+    case 2: std::cout << "Tuesday"; break;
+    case 3: std::cout << "Wednesday"; break;
+    default: std::cout << "Invalid";
+}
+// Output: Wednesday
+*/
+
+
+
+
+// 5. Ternary operator 
+/*
+syntax :- 
+condition ? expression_if_true : expression_if_false;
+
+
+
+int age = 20;
+std::string result = (age >= 18) ? "Adult" : "Minor";
+std::cout << result;
+// Output: Adult
+*/
