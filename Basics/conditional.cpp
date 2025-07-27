301078f9e312ac70538cf4549114d2ddd087b232
Karan Nayal <somnathtigers2006@gmail.com>
2025-07-27 13:34:49 +0530

    code executed in conditional.cpp

--- Basics/conditional.cpp
+++ Basics/conditional.cpp
@@ -43,10 +43,10 @@ if (age >= 18) {
 /*
 int day = 3;
 switch (day) {
-    case 1: std::cout << "Monday"; break;
-    case 2: std::cout << "Tuesday"; break;
-    case 3: std::cout << "Wednesday"; break;
-    default: std::cout << "Invalid";
+    case 1: std::cout << "Monday" ; break ;
+    case 2: std::cout << "Tuesday" ; break ;
+    case 3: std::cout << "Wednesday" ; break ;
+    default: std::cout << "Invalid" ;
 }
 // Output: Wednesday
 */
