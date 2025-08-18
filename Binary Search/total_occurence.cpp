fc3f3dd350f6136531f272f7dbf1f10d3e8be248
Karan Nayal <somnathtigers2006@gmail.com>
2025-08-19 01:03:42 +0530

    Code executed

---
+++ Binary Search/total_occurence.cpp
@@ -0,0 +1,63 @@
+// find the total occurence of the key 
+#include<iostream>
+using namespace std ; 
+
+int lastoccurence(int arr[] , int n , int key) {
+    int s=0 ; 
+    int e=n-1 ; 
+    int ans = -1 ; 
+    int mid = s + (e-s)/2 ;
+    while(s <= e) {
+        if(arr[mid] == key) {
+            ans = mid ; 
+            s = mid + 1 ; 
+        } else if(key > arr[mid]) {
+            s = mid + 1; // Search in the right half
+        } else {
+            e = mid - 1; // Search in the left half
+        }
+        mid = s + (e-s)/2; // Update mid
+    }
+    return ans ; 
+}
+
+int firstoccurence(int arr[] , int n , int key) {
+    int s=0 ; 
+    int e=n-1 ; 
+    int ans = -1 ; 
+    int mid = s + (e-s)/2 ;
+    while(s <= e) {
+        if(arr[mid] == key) {
+            ans = mid ; 
+            e = mid - 1 ; 
+        } else if(key > arr[mid]) {
+            s = mid + 1; // Search in the right half
+        } else {
+            e = mid - 1; // Search in the left half
+        }
+        mid = s + (e-s)/2; // Update mid
+    }
+    return ans ; 
+}
+
+int main() {
+    int n ; 
+    cout<<"Enter the size of an array : " ; 
+    cin>>n;
+
+    int arr[n] ;
+    cout<<"Enter the elements of the array in sorted order : \n" ;
+    for(int i=0 ; i<n ; i++) {
+        cin >> arr[i] ;
+    }
+
+    int key ;
+    cout<<"Enter the key : " ; 
+    cin >> key ;
+
+    int last = lastoccurence(arr , n , key) ; 
+    int first = firstoccurence(arr , n , key) ; 
+    cout<<"Total number of occurence : "<< (last - first + 1) << endl;
+
+    return 0 ; 
+}
