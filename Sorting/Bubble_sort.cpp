17c5df1fa025ef4571e3a2b3a4da07343dd3ed7c
Karan Nayal <somnathtigers2006@gmail.com>
2025-08-24 15:53:16 +0530

    Code added

---
+++ Sorting/Bubble_sort.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std ; 
+
+int bubblesort(int arr[] , int n) {
+    for(int i=1 ; i<n ; i++) {
+        for(int j=0 ; j<n-i ; j++) {
+            if(arr[j] > arr[j+1]) {
+                swap(arr[j] , arr[j+1]) ;
+            }
+        }
+    }
+}
+
+
+int main() {
+    int n ; 
+    cout<<"Enter the size of an array : " ; 
+    cin>> n ;
+
+    int arr[n] ;
+    cout<<"Enter the elements of an array : " ;
+    for(int i=0 ; i<n ; i++ ) {
+        cin>> arr[i] ;
+    }
+
+    bubblesort(arr , n) ;
+    
+    cout<<"Sorted array : "<<endl ; 
+    for(int i=0 ; i<n ; i++ ) {
+        cout<< arr[i] << " " ;
+    }
+
+    return 0 ; 
+}
