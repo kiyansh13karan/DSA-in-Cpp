a53eb76f708238365fa86d39bdaed1605d33ce2c
Karan Nayal <somnathtigers2006@gmail.com>
2025-08-31 11:55:12 +0530

    Code added

--- STL/algorithm.cpp
+++ STL/algorithm.cpp
@@ -17,56 +17,62 @@ int main() {
     for (int x : v) cout << x << " ";
     cout << endl;
 
-    // 2. sort (descending)
+    // 2. Rotate
+    rotate(v.begin(), v.begin() + 2, v.end());
+    cout << "Rotated (left by 2): ";
+    for (int x : v) cout << x << " ";
+    cout << endl;
+
+    // 3. sort (descending)
     sort(v.begin(), v.end(), greater<int>());
     cout << "Sorted (desc): ";
     for (int x : v) cout << x << " ";
     cout << endl;
 
-    // 3. reverse
+    // 4. reverse
     reverse(v.begin(), v.end());
     cout << "Reversed: ";
     for (int x : v) cout << x << " ";
     cout << endl;
 
-    // 4. min/max element
+    // 5. min/max element
     cout << "Min element: " << *min_element(v.begin(), v.end()) << endl;
     cout << "Max element: " << *max_element(v.begin(), v.end()) << endl;
 
-    // 5. count occurrences
+    // 6. count occurrences
     cout << "Count of 5: " << count(v.begin(), v.end(), 5) << endl;
 
-    // 6. find
+    // 7. find
     auto it = find(v.begin(), v.end(), 2);
     if (it != v.end())
         cout << "Element 2 found at index: " << distance(v.begin(), it) << endl;
     else
         cout << "Element 2 not found" << endl;
 
-    // 7. accumulate (sum)
+    // 8. accumulate (sum)
     int sum = accumulate(v.begin(), v.end(), 0);
     cout << "Sum of elements: " << sum << endl;
 
-    // 8. binary_search (needs sorted vector)
+    // 9. binary_search (needs sorted vector)
     sort(v.begin(), v.end()); // sort again
     cout << "Binary search for 3: " 
          << (binary_search(v.begin(), v.end(), 3) ? "Found" : "Not Found") 
          << endl;
 
-    // 9. lower_bound & upper_bound
+    // 10. lower_bound & upper_bound
     auto lb = lower_bound(v.begin(), v.end(), 3);
     auto ub = upper_bound(v.begin(), v.end(), 3);
     cout << "Lower bound of 3: " << (lb != v.end() ? *lb : -1) << endl;
     cout << "Upper bound of 3: " << (ub != v.end() ? *ub : -1) << endl;
 
-    // 10. next_permutation
+    // 11. next_permutation
     vector<int> p = {1, 2, 3};
     next_permutation(p.begin(), p.end());
     cout << "Next permutation of {1,2,3}: ";
     for (int x : p) cout << x << " ";
     cout << endl;
 
-    // 11. prev_permutation
+    // 12. prev_permutation
     prev_permutation(p.begin(), p.end());
     cout << "Previous permutation: ";
     for (int x : p) cout << x << " ";
