dcd02d9bb98d02b9e14a07b862f9d32f97a1e2b2
Karan Nayal <somnathtigers2006@gmail.com>
2025-12-30 09:19:56 +0530

    code updated

--- Char Array/basic.cpp
+++ Char Array/basic.cpp
@@ -14,7 +14,7 @@ int main() {
     char name2[20] ; 
     cout<<"Enter your name2 : " ; 
     cin.getline(name2 , 20) ; 
-    cout<<"Hello "<<name2<<endl ;
+    cout<<"Hello "<<name2<<endl ; 
 
     // length of name2 char array 
     cout<<"Lenght  of your name2 is : "<<strlen(name2)<<endl ; 
